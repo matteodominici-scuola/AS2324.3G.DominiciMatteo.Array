351951fb8b0460355f7930eb170ce132022fe3b2
Matteo Dominici <dominicimatteo07@gmail.com>
2024-02-22 09:02:19 +0100

    Add project files.

---
+++ AS2324.3G.DominiciMatteo.Array/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AS2324._3G.DominiciMatteo.Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
