9de308753c306bdb409d42f55bcfd922dc59561a
Matteo Dominici <dominicimatteo07@gmail.com>
2024-02-22 10:21:49 +0100

    feat funzione che calcola la media dei pesi, il peso minimo e il peso massimo e li stampa a schermo. fix della prima funzione che dà randomicamente dei pesi e delle età alle persone

--- AS2324.3G.DominiciMatteo.Array/Program.cs
+++ AS2324.3G.DominiciMatteo.Array/Program.cs
@@ -6,19 +6,45 @@
         {
             Console.WriteLine("Matteo Dominici; 3G; 22/02/2024; Esercizio Array");
             Console.WriteLine("Quante persone vuoi inserire?");
-            int dim = Convert.ToInt32(Console.ReadLine());
-            double[] pesi = new double[dim];
-            int[] eta = new int[dim];
+            int numPersone = Convert.ToInt32(Console.ReadLine());
+            double[] pesi = new double[numPersone];
+            int[] eta = new int[numPersone];
+            double min = 0;
+            double max = 0;
+            double media = 0;
             CaricaVettori(ref pesi, ref eta);
+            Statistiche(ref pesi, ref media, ref min, ref max);
+            Console.WriteLine($"Media pesi: {media}");
+            Console.WriteLine($"Peso minimo: {min}");
+            Console.WriteLine($"Peso massimo: {max}");
         }
         static void CaricaVettori(ref double[] pesi, ref int[] eta)
         {
-            foreach (int i in pesi)
+            Random rnd = new Random();
+            for(int i= 0; i < pesi.Length; i++)
             {
-                Random rnd = new Random();
                 pesi[i] = rnd.Next(50, 101);
                 eta[i] = rnd.Next(18, 100);
             }
+        }
+        static void Statistiche(ref double[] pesi, ref double media, ref double min, ref double max)
+        {
+            double sommaPesi = 0;
+            min = pesi[0];
+            max = pesi[0];
+            foreach(double peso in pesi)
+            {
+                sommaPesi += peso;
+                if (peso > max)
+                {
+                    max = peso;
+                }
+                if (peso < min)
+                {
+                    min = peso;
+                }
+            }
+            media = sommaPesi / pesi.Length;
         }
     }
 }
