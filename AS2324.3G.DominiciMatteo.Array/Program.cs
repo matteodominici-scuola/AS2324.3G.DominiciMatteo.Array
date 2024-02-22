7a74efa05c6c9dcb2870992083bbe81a973248ed
Matteo Dominici <dominicimatteo07@gmail.com>
2024-02-22 09:21:51 +0100

    fit funzione che inserisce randomicamente i pesi e le età.

--- AS2324.3G.DominiciMatteo.Array/Program.cs
+++ AS2324.3G.DominiciMatteo.Array/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Matteo Dominici; 3G; 22/02/2024; Esercizio Array");
+            Console.WriteLine("Quante persone vuoi inserire?");
+            int dim = Convert.ToInt32(Console.ReadLine());
+            double[] pesi = new double[dim];
+            int[] eta = new int[dim];
+            CaricaVettori(ref pesi, ref eta);
+        }
+        static void CaricaVettori(ref double[] pesi, ref int[] eta)
+        {
+            foreach (int i in pesi)
+            {
+                Random rnd = new Random();
+                pesi[i] = rnd.Next(50, 101);
+                eta[i] = rnd.Next(18, 100);
+            }
         }
     }
 }
+
